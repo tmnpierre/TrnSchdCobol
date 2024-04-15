c9b0fba27689e2e8ed0838ad78a16e804a448e93
tmnpierre <tomyn.pierre@gmail.com>
2024-04-15 16:55:38 +0200

    initial commit

---
+++ TrnSchd.cbl
@@ -0,0 +1,47 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. TrnSchd.
+       AUTHOR. Pierre.
+
+       ENVIRONMENT DIVISION.
+       INPUT-OUTPUT SECTION.
+       FILE-CONTROL.
+           SELECT TRAIN-FILE ASSIGN TO 'train.dat'
+               ORGANIZATION IS LINE SEQUENTIAL
+               ACCESS MODE IS SEQUENTIAL.
+
+       DATA DIVISION.
+       FILE SECTION.
+       FD TRAIN-FILE
+           RECORD IS VARYING IN SIZE FROM 19 TO 29 CHARACTERS
+               DEPENDING ON TRAIN-RECORD-LENGTH.
+       01 TRAIN-RECORD.
+           05 TRAIN-TYPE          PIC X(3).
+           05 DESTINA         PIC X(7).
+           05 TRAIN-TIME.
+               10 TRAIN-HOUR      PIC 99.
+               10 TRAIN-MINUTES   PIC 99.
+           05 NUMBER-OF-STOPS     PIC X(12).
+
+       WORKING-STORAGE SECTION.
+       01  TRAIN-RECORD-LENGTH   PIC 9(2) COMP.
+       01  WS-END-OF-FILE        PIC X VALUE 'N'.
+           88 EOF               VALUE 'Y'.
+           88 NOT-EOF           VALUE 'N'.
+
+       PROCEDURE DIVISION.
+           OPEN INPUT TRAIN-FILE
+           PERFORM UNTIL EOF
+               READ TRAIN-FILE INTO TRAIN-RECORD
+                   AT END
+                       MOVE 'Y' TO WS-END-OF-FILE
+                   NOT AT END
+                       DISPLAY "Train Type: " TRAIN-TYPE
+                       DISPLAY "Destination: " DESTINA
+                       DISPLAY "Train Time: " TRAIN-HOUR ":" 
+                               TRAIN-MINUTES
+                       DISPLAY "Number of Stops: " NUMBER-OF-STOPS
+                       DISPLAY "------------------------------"
+               END-READ
+           END-PERFORM
+           CLOSE TRAIN-FILE.
+           STOP RUN.
